406f706338ed7daea585302c7ff0cb43a062b8b9
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-02 12:28:08 +0600

    Full code

--- NodeJS/Module/Built_in_Module/index.js
+++ NodeJS/Module/Built_in_Module/index.js
@@ -1,52 +1,53 @@
 const fs = require("fs");
+console.log(fs);
 
-// fs.writeFile("demo1.txt", "My name is subrata.", (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successful");
-//   }
-// });
+fs.writeFile("demo1.txt", "My name is subrata.", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successful");
+  }
+});
 
-// fs.appendFile("demo1.txt", "My age is 25 years old", (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successful");
-//   }
-// });
+fs.appendFile("demo1.txt", "My age is 25 years old", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successful");
+  }
+});
 
-// fs.readFile("demo1.txt", "utf-8", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(data);
-//   }
-// });
+fs.readFile("demo1.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
 
-// fs.rename("demo1.txt", "demo2.txt", (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successfull");
-//   }
-// });
+fs.rename("demo1.txt", "demo2.txt", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfull");
+  }
+});
 
-// fs.unlink("demo2.txt", (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successfull");
-//   }
-// });
+fs.unlink("demo2.txt", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfull");
+  }
+});
 
-// fs.exists("demo2.txt", (result) => {
-//   if (result) {
-//     console.log("File exist.");
-//   } else {
-//     console.log("Can not found the file");
-//   }
-// });
+fs.exists("demo2.txt", (result) => {
+  if (result) {
+    console.log("File exist.");
+  } else {
+    console.log("Can not found the file");
+  }
+});
 
 const res = fs.existsSync("demo2.txt");
 

670e2f0d527583bfe07fcf0397889cda6f693643
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-02 12:16:33 +0600

    docs: Rename the file using rename function of fs module.

--- NodeJS/Module/Built_in_Module/index.js
+++ NodeJS/Module/Built_in_Module/index.js
@@ -16,10 +16,18 @@ const fs = require("fs");
 //   }
 // });
 
-fs.readFile("demo1.txt", "utf-8", (err, data) => {
+// fs.readFile("demo1.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+fs.rename("demo1.txt", "demo2.txt", (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log("Successfull");
   }
 });

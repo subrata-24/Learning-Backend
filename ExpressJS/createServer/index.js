8b72242b4794738fe63af22e41a55ea897ea1ded
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-06 03:35:21 +0600

    feat: add GET routes for Home and About pages
    - Implemented GET '/' route to serve home page message
    - Implement GET '/about' route to serve about page message

--- ExpressJS/createServer/index.js
+++ ExpressJS/createServer/index.js
@@ -1,6 +1,14 @@
 const app = require("./app");
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("I am home page!!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("I am abot page.");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });

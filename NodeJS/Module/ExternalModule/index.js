c80556365ee35c31d226b97443380c78ec78d217
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-05 12:18:14 +0600

    learn about useing external module

---
+++ NodeJS/Module/ExternalModule/index.js
@@ -0,0 +1,7 @@
+const randomFruiteName = require("random-fruits-name");
+
+console.log(randomFruiteName("nl"));
+
+const moviesNames = require("movies-names");
+
+console.log(moviesNames.random(3));

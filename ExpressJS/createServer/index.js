7c0268fa442194f027e137e6f2c96485482f2427
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-06 03:44:52 +0600

    feat:Add post, put, delete method - post: To post something on database - put: To update some existinng things - delete: To delete something from database

--- ExpressJS/createServer/index.js
+++ ExpressJS/createServer/index.js
@@ -9,6 +9,18 @@ app.get("/about", (req, res) => {
   res.send("I am abot page.");
 });
 
+app.post("/", (req, res) => {
+  console.log("Post something on home page");
+});
+
+app.put("/", (req, res) => {
+  console.log("Update on home page.");
+});
+
+app.delete("/", (req, res) => {
+  console.log("Delete something from home page.");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });

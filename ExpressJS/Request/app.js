8abae017811a4f7a32125befcaaabf368daa9d18
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-06 22:09:55 +0600

    feat: add POST route to receive client info from request body
    - Added body-parser middleware for URL-encoded and JSON request bodies
    - Implemented POST '/user' route to read 'name' and 'id' from req.body
    - Sends dynamic response with student name and ID

--- ExpressJS/Request/app.js
+++ ExpressJS/Request/app.js
@@ -1,19 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
+// Middleware to parse URL-encoded form data (from HTML forms)
+// Example: <form method="POST" action="/user">...</form>
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to parse JSON data sent in the request body
+// Example: POST request with Content-Type: application/json
+app.use(bodyParser.json());
+
+// POST route to handle "/user" requests
+app.post("/user", (req, res) => {
   /*
-    Access HTTP request headers using req.header("headerName")
-    - "name" and "id" are expected to be sent as headers in the 
-    request by the client.
-    Example: 
-      name: Subrata
-      id: 24
+    Access POST request data from req.body
+    - "name" and "id" are expected in the request body
+    Example JSON body: { "name": "Subrata", "id": "24" }
   */
-  const name = req.header("name");
-  const id = req.header("id");
-  res.send(`<h1>Student name is ${name} and id is ${id}</h1>`);
+  const name = req.body.name;
+  const id = req.body.id;
+
+  /*
+    Send a dynamic response displaying student name and ID
+    Example response: "Student name is Subrata and id is 24"
+  */
+  res.send(`Student name is ${name} and id is ${id}`);
 });
 
 module.exports = app;

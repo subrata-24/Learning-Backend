159b4d28b815f98fac4c32b5c7973da539c5a220
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-06 21:31:32 +0600

    feat: add GET route reading client info from request headers
    - Implement GET '/' route that reads 'name' and 'id' from HTTP headers
    - Send dynamic HTML response displaying student name and ID

--- ExpressJS/Request/app.js
+++ ExpressJS/Request/app.js
@@ -2,17 +2,17 @@ const express = require("express");
 
 const app = express();
 
-/*
-  Route with dynamic URL parameters
-  URL pattern: /userName/:name/userId/:id
-  - :name and :id are route parameters (mandatory)
-  - They allow passing values directly in the URL path
-  Example URL: http://localhost:3000/userName/Subrata/userId/24
-*/
-app.get("/userName/:name/userId/:id", (req, res) => {
-  // Extract route parameters from req.params object
-  // req.params = { name: "Subrata", id: "24" } for the example URL
-  const { name, id } = req.params;
+app.get("/", (req, res) => {
+  /*
+    Access HTTP request headers using req.header("headerName")
+    - "name" and "id" are expected to be sent as headers in the 
+    request by the client.
+    Example: 
+      name: Subrata
+      id: 24
+  */
+  const name = req.header("name");
+  const id = req.header("id");
   res.send(`<h1>Student name is ${name} and id is ${id}</h1>`);
 });
 

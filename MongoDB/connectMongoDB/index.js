b5603649351e43e665659baca893e6446dcbfd02
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-13 12:27:58 +0600

    feate: Learn about schema and model.

--- MongoDB/connectMongoDB/index.js
+++ MongoDB/connectMongoDB/index.js
@@ -5,13 +5,25 @@ const PORT = 3000;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect("mongodb://127.0.0.1:27017/ProductDB");
     console.log("Database connected successfully.");
   } catch (error) {
     console.log("Something wrong to  connect DB.");
     console.log(error);
   }
 };
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  Description: String,
+  Date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const products = mongoose.model("products", productSchema);
 
 app.get("/", (req, res) => {
   res.send(`<h1>Try to connect mongoDB</h1>`);
